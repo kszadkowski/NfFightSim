dbe9d83fb4e104c2193a05c335c03698017e17ff
Kazimierz Szadkowski <kazimierz.szadkowski@gmail.com>
2019-04-23 21:29:14 +0200

    initialization

---
+++ NFFightSim/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+
+namespace NFFightSim.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ViewResult Highlander()
+		{
+			ViewBag.Title = "Highlander";
+			return View();
+		}
+
+		public ViewResult Kingslayers()
+		{
+			ViewBag.Title = "Kingslayers";
+			return View();
+		}
+
+
+		public ViewResult WeaponMaster()
+		{
+			ViewBag.Title = "WeaponMaster";
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
